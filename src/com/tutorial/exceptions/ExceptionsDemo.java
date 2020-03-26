653eb94fdf7bce6460246cf40142c76391128965
hassan.adel <hasanadel555@gmail.com>
2020-03-26 03:31:55 +0200

    catch multiple exceptions

--- src/com/tutorial/exceptions/ExceptionsDemo.java
+++ src/com/tutorial/exceptions/ExceptionsDemo.java
@@ -2,6 +2,9 @@ package com.tutorial.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo
 {
@@ -14,9 +17,15 @@ public class ExceptionsDemo
     public static void showCheckedException() {
         try {
             var reader = new FileReader("");
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("File doesn't exist!");
+        } catch (IOException e) {
+            System.out.println("cannot read data!");
+        } catch (ParseException e) {
+            System.out.println("cannot parse date");
         }
         System.out.println("File opened");
     }
