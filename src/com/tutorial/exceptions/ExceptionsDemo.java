5dbc97bc4024d7458cef6717523424507dbaf2c4
hassan.adel <hasanadel555@gmail.com>
2020-03-28 02:16:51 +0200

    The try-with-resources Statement

--- src/com/tutorial/exceptions/ExceptionsDemo.java
+++ src/com/tutorial/exceptions/ExceptionsDemo.java
@@ -2,6 +2,7 @@ package com.tutorial.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -15,24 +16,16 @@ public class ExceptionsDemo
 
     //Checked exception because java compiler checks them in runtime
     public static void showCheckedException() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("");
+        try (
+            var reader = new FileReader("");
+            var writter = new FileWriter("...")
+        ) {
             var value = reader.read();
             new SimpleDateFormat().parse("");
         } catch (IOException | ParseException e) {
             System.out.println("cannot read data!");
         }
-        // will always get executed whether we have an exception or not
-        finally {
-            if(reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+
         System.out.println("File opened");
     }
 
